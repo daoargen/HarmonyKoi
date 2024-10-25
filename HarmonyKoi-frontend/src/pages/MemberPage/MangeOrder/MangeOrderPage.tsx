f5e1308e10875e9b85c7c4362ca0bd611004f35e
daoargen <daolvse172121@fpt.edu.vn>
2024-10-25 17:44:30 +0700

    fix order manage page

--- HarmonyKoi-frontend/src/pages/MemberPage/MangeOrder/MangeOrderPage.tsx
+++ HarmonyKoi-frontend/src/pages/MemberPage/MangeOrder/MangeOrderPage.tsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const ManageOrder: React.FC = () => {
-  return <h1>Trang Order</h1>
-}
-
-export default ManageOrder
-=======
 'use client'
 
 import React, { useState, useEffect } from 'react'
@@ -195,4 +186,3 @@ const ManageOrderPage: React.FC = () => {
 }
 
 export default ManageOrderPage
->>>>>>> 3cdbdda66d4956c74990185e2c3f314b3c73d19d
