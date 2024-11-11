11e1a0f5009200e1cae69e024ebcd866149691bd
daoargen <daolvse172121@fpt.edu.vn>
2024-11-11 21:49:23 +0700

    fix time check cancel order

--- HarmonyKoi-backend/src/services/scheduleTask.service.ts
+++ HarmonyKoi-backend/src/services/scheduleTask.service.ts
@@ -2,10 +2,11 @@ import cron from "node-cron"
 
 import orderService from "~/services/order.service"
 
-const cronExpression = "*/5 * * * *"
+const cronExpression = "0 * * * *"
 
 export const initializeSchedulers = () => {
   cron.schedule(cronExpression, () => {
     orderService.cancelPendingOrders()
+    console.log(new Date())
   })
 }
